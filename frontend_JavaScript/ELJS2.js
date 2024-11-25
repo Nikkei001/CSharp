cf40cf42d66b38e30e199fc06da24e073146d505
Nikkei001 <santates001@gmail.com>
2024-11-25 18:57:04 +0800

    visual studio code committed

--- frontend_JavaScript/ELJS2.js
+++ frontend_JavaScript/ELJS2.js
@@ -34,4 +34,53 @@ for (let num = 1; num <= 4; num++) {
 
 console.log("=".repeat(100));
 
+// 画一个三角形
+// my solution
+let string = "#"
+for (let count = 0; count <= 7; count++) {
+    console.log(string.repeat(count));
+}
+// my solution2
+for (let string2 = "#"; string2.length <= 7; string2 = string2 + "#") {
+    console.log(string2);
+}
+// 作者的解法string2 += "#"
+
+console.log("=".repeat(100));
+
+// 打印1到100, 能被3整除的数字打印"Fizz",能被5整除的数字打印"Buzz"
+// 打印1到100, 能同时被3和5整除的数字打印"FizzBuzz"
+for (let index2 = 1; index2 <= 100; index2++) {
+    if (index2 % 3 === 0 && index2 % 5 === 0) console.log("FizzBuzz")
+    else if (index2 % 3 === 0) console.log("Fizz")
+    else if (index2 % 5 === 0) console.log("Buzz")
+    else console.log(index2)
+}
+
+console.log("=".repeat(100));
+
+// 作者的解法(借鉴学习)
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+}
+
+console.log("=".repeat(100));
+
+// 输出一个棋盘,找规律,注意运算符的优先级
+for (let row = 1; row <= 8; row++) {
+    let rst = "";
+    for (let column = 1; column <= 8; column++) {
+        if ((row + column) % 2 === 0) rst += " ";
+        if ((row + column) % 2 !== 0) rst += "#";
+    }
+    rst += "\n";
+    console.log(rst);
+}
+
+
+
+
 
